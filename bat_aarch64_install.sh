81e9d1cdabe9f635a06bddb749d68794a9d43642
chris <chris@localhost.localdomain>
2023-10-05 13:57:46 -0400

    bat_aarch64_install.sh

---
+++ bat_aarch64_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a simple bash script that will automate the installation of 'bat' on your CentOS system
+# Please make sure to run it with 'sudo' right to prevent any permission conflicts during the installation.
+# Currently it is only working for aarch64 systems, however I might add compatibility for other architectures soon.
+
+# [ Pulling the latest binary release from the 'bat' repository on GitHub ]
+
+wget 
+
